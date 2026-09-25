3f8aac1d2ecfe879d8b0154104e8a536d83cb24f
creationdebot <ww2743928@gmail.com>
2026-09-25 23:17:20 +0200

    Create clode.js

---
+++ commands/clode.js
@@ -0,0 +1,15 @@
+const { errorEmbed, successEmbed } = require('../utils');
+
+module.exports = {
+  name: 'clode',
+  description: 'Ferme le ticket dans lequel la commande est utilisee.',
+  usage: '&clode',
+  async execute(message) {
+    if (!message.channel.topic?.startsWith('ticket:')) {
+      return message.reply({ embeds: [errorEmbed('Cette commande ne fonctionne que dans un salon de ticket.')] });
+    }
+
+    await message.reply({ embeds: [successEmbed('🔒 Ce ticket sera ferme dans 5 secondes...')] });
+    setTimeout(() => message.channel.delete().catch(() => {}), 5000);
+  },
+};
