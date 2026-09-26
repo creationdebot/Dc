c788c1eb56beb21647ceb829341b048206d90b5b
creationdebot <ww2743928@gmail.com>
2026-09-26 04:19:13 +0200

    Update and rename clode.js to close.js

--- commands/close.js
+++ commands/close.js
@@ -1,15 +1,15 @@
 const { errorEmbed, successEmbed } = require('../utils');
 
 module.exports = {
-  name: 'clode',
+  name: 'close',
   description: 'Ferme le ticket dans lequel la commande est utilisee.',
-  usage: '&clode',
+  usage: '&close',
   async execute(message) {
     if (!message.channel.topic?.startsWith('ticket:')) {
       return message.reply({ embeds: [errorEmbed('Cette commande ne fonctionne que dans un salon de ticket.')] });
     }
 
-    await message.reply({ embeds: [successEmbed('🔒 Ce ticket sera ferme dans 5 secondes...')] });
+    await message.reply({ embeds: [successEmbed('🔒 Ce ticket sera ferme dans 5 secondes dcp tg nn arrete de parler alors que le ticket ce ferme ')] });
     setTimeout(() => message.channel.delete().catch(() => {}), 5000);
   },
 };
